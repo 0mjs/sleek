303f94792f0abfe716a98a8742e90a1ed251bfee
0mjs <mail@mattjs.me>
2024-02-14 18:03:42 +0000

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="zephyr",
+    name="sleek",
     version="0.0.1",
-    description="A lightweight ASGI framework for Python",
     author="Matt J. Stevenson",
     author_email="dev@mattjs.me",
+    description="A minimalistic, ASGI, Python framework for building REST API's.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/0mjs/sleek",
     packages=find_packages(),
-    install_requires=["httpx", "uvicorn", "starlette", "pydantic"],
+    install_requires=[
+        "uvicorn",
+        "starlette",
+        "httpx",
+        "pydantic",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.11",
 )
