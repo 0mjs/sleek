9423f45d0f2394c993180279ccf1b216d3342743
0mjs <mail@mattjs.me>
2024-02-15 17:15:50 +0000

    fix:v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sleekify",
-    version="0.0.8",
+    version="0.0.9",
     author="Matt J. Stevenson",
     author_email="dev@mattjs.me",
     description="A minimalistic, ASGI, Python framework for building REST API's.",
